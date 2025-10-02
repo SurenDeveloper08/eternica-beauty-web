b3efa7b49ed6ec802085d1bfb60e2e4eb768bbba
ADMIN <surendirandeveloper@gmail.com>
2025-10-02 16:05:17 +0400

    image magnify

--- src/components/ImageZoom/ImageZoom.js
+++ src/components/ImageZoom/ImageZoom.js
@@ -1,18 +1,39 @@
 import React from "react";
-import InnerImageZoom from "react-inner-image-zoom";
-import "react-inner-image-zoom/lib/InnerImageZoom/styles.css";
-import './ImageZoom.css'
-const ImageZoom = ({ src, alt }) => {
+import ReactImageMagnify from "react-image-magnify";
 
+const ImageZoom = ({ src, alt }) => {
   return (
     <div style={{ maxWidth: "400px", margin: "0 auto" }}>
-      <InnerImageZoom
-        src={src}
-        zoomSrc={src}
-        alt={alt || "Product Image"}
-        zoomType="hover"
-        zoomPreload={true}
-        className="rounded-md shadow-md bg-white"
+      <ReactImageMagnify
+        {...{
+          smallImage: {
+            alt: alt || "Product Image",
+            isFluidWidth: true,
+            src: src,
+            style: {
+                backgroundColor: "#fff",
+                borderRadius: "8px",
+                maxHeight: "500px",
+                objectFit: "contain",
+              },
+          },
+          largeImage: {
+            src: src,
+            width: 1200,
+            height: 1800,
+            
+          },
+          enlargedImageContainerStyle: {
+              zIndex: 9999,
+              background: "#fff",
+              borderRadius: "0.375rem",
+              boxShadow: "0 6px 20px rgba(0,0,0,0.15)",
+            },
+            enlargedImageContainerDimensions: {
+              width: "150%",
+              height: "120%",
+            },
+        }}
       />
     </div>
   );
