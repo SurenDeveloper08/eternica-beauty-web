909b2c799dd2a55c3c141c10e36bc189f2a4c2e8
ADMIN <surendirandeveloper@gmail.com>
2025-10-02 17:16:44 +0400

    routes hide

--- src/routes/WebRoutes.js
+++ src/routes/WebRoutes.js
@@ -3,14 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import Home from '../pages/Home/Home';
-import ProductListing from '../pages/ProductListing/ProductListing';
+// import ProductListing from '../pages/ProductListing/ProductListing';
 import WhatsAppButton from '../components/WhatsAppButton/WhatsAppButton';
-import Product from '../pages/Product/Product';
-import Cart from '../pages/Cart/Cart';
-import Checkout from '../pages/Checkout/Checkout';
-import ThankYouPage from '../components/ThankYouPage/ThankYouPage';
-import ContactUs from '../pages/ContactUs/ContactUs';
-import AboutUs from '../components/AboutUs/AboutUs ';
+// import Product from '../pages/Product/Product';
+// import Cart from '../pages/Cart/Cart';
+// import Checkout from '../pages/Checkout/Checkout';
+// import ThankYouPage from '../components/ThankYouPage/ThankYouPage';
+// import ContactUs from '../pages/ContactUs/ContactUs';
+// import AboutUs from '../components/AboutUs/AboutUs ';
 
 const WebsiteRoutes = () => {
 
@@ -19,14 +19,14 @@ const WebsiteRoutes = () => {
             <Header />
             <Routes>
                 <Route index element={<Home />} />
-                <Route path=":category" element={<ProductListing />} />
+                {/* <Route path=":category" element={<ProductListing />} />
                 <Route path=":category/:subCategory" element={<ProductListing />} />
                 <Route path=":category/:subCategory/:slug" element={<Product />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/checkout" element={<Checkout />} />
                 <Route path="/thank-you" element={<ThankYouPage />} />
                 <Route path="/contact" element={<ContactUs />} />
-                <Route path="/about" element={<AboutUs />} />
+                <Route path="/about" element={<AboutUs />} /> */}
             </Routes>
             <Footer />
             <WhatsAppButton />
